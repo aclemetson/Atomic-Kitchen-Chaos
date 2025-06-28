b3fd4a5e6f49fcc62da1831146abf2fd7b298d13
Anthony Clemetson <clemtek1@gmail.com>
2025-06-28 10:57:58 -0700

    Cleaning up for .png temporarily

---
+++ Assets/Scripts/GeneratedObjects/LevelSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace AtomicKitchenChaos.GeneratedObjects
+{
+    public class LevelSO : ScriptableObject
+    {
+        public string levelName;
+        public RecipeListSO recipeListSO;
+    }
+}
---
+++ Assets/Scripts/Level/LevelData.cs
@@ -0,0 +1,8 @@
+namespace AtomicKitchenChaos.Level
+{
+    public struct LevelData 
+    {
+        public string Name { get; set; }
+
+    }
+}
